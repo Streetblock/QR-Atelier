31194619097b97a96765209961f9e7a8dee9c351
Streetblock <github@streetblock.de>
2026-06-18 19:14:19 +0200

    chore: remove aztec style field

--- app.js
+++ app.js
@@ -35,10 +35,8 @@ class QRPlaygroundApp {
       primaryInputLabel: document.getElementById('primary-input-label'),
       primaryInputField: document.getElementById('field-primary-input'),
       dotShape: document.getElementById('dot-shape'),
-      aztecStyle: document.getElementById('aztec-style'),
       cornerShape: document.getElementById('corner-shape'),
       dotShapeField: document.getElementById('field-dot-shape'),
-      aztecStyleField: document.getElementById('field-aztec-style'),
       cornerShapeField: document.getElementById('field-corner-shape'),
       wifiAuth: document.getElementById('wifi-auth'),
       wifiPassword: document.getElementById('wifi-password'),
@@ -234,7 +232,6 @@ class QRPlaygroundApp {
     if (this.ui.wifiPassword) this.ui.wifiPassword.disabled = isWifi && this.state.options.wifiAuth === 'nopass'
 
     if (this.ui.dotShapeField) this.ui.dotShapeField.classList.toggle('hidden', isMaxi)
-    if (this.ui.aztecStyleField) this.ui.aztecStyleField.classList.add('hidden')
     if (this.ui.cornerShapeField) this.ui.cornerShapeField.classList.toggle('hidden', isMaxi)
     if (this.ui.logoUploadField) this.ui.logoUploadField.classList.toggle('hidden', isMaxi)
     if (this.ui.wifiSection) {
